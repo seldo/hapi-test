6ad3ac89b9311b278d509b133de516d4b0a0b8ee
Laurie Voss <git@seldo.com>
2014-03-02 22:19:48 -0800

    Achievement unlocked: infinite scroll!

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,92 +1,159 @@
 var async = require('async')
 
+var getItem = function(request,slug,cb) {
+
+  console.log("slug is " + slug)
+
+  request.server.plugins['hapi-mysql'].pool.getConnection(function(err, connection) {
+    if (err) {
+      console.log(err)
+      cb("DB connection failed (0003)")
+      return
+    }
+
+    // what we want is this item, and the item the comes immediately after it chronologically
+    var sql = "select * from content " +
+      "where published <= " +
+      "(select published from content " +
+      "where slug = ? ) " +
+      "order by published desc limit 2";
+
+    console.log(sql)
+    console.log(slug)
+
+    // Use the connection
+    connection.query(
+      sql,
+      [slug],
+      function(er, rows) {
+
+        console.log("queried stuff")
+
+        if(er) {
+          cb( "content query failed (0001)")
+        } else {
+          if (rows.length == 0) {
+            cb("no content found (0002)")
+          } else {
+            var result = rows[0]
+            result.nextSlug = rows[1].slug
+            cb(null,result)
+          }
+        }
+
+        // And done with the connection.
+        connection.release()
+      }
+    )
+  })
+
+
+}
+
+/*
+var getLatest = function(request,reply,cb) {
+  request.server.plugins['hapi-mysql'].pool.getConnection(function(err, connection) {
+    if (err) {
+      console.log(err)
+      cb("DB connection failed (0003)")
+      return
+    }
+
+    var sql = "select * from content order by created desc limit 1"
+
+    // Use the connection
+    connection.query(
+      sql,
+      // bindValues,
+      function(er, rows) {
+
+        console.log("queried stuff")
+
+        if(er) {
+          cb( "content query failed (0001)")
+        } else {
+          if (rows.length == 0) {
+            cb("no content found (0002)")
+          } else {
+            var result = rows[0]
+            cb(null,result)
+          }
+        }
+
+        // And done with the connection.
+        connection.release()
+      }
+    )
+  })
+}
+*/
+
+var getIndex = function(request,reply,cb) {
+  console.log("index")
+  request.server.plugins['hapi-mysql'].pool.getConnection(function(err, connection) {
+
+    if (err) {
+      cb("DB connection failed (0004)")
+      return
+    }
+
+    var sql = "select title, slug, published from content order by created desc limit 50"
+
+    // Use the connection
+    connection.query(
+      sql,
+      // bindValues,
+      function(er, rows) {
+
+        if(er) {
+          cb( "content query failed (0005)")
+        } else {
+          if (rows.length == 0) {
+            cb("no content found (0006)")
+          } else {
+            cb(null,rows)
+          }
+        }
+
+        // And done with the connection.
+        connection.release()
+      }
+    )
+  })
+
+}
+
+
 exports.index = function(request,reply) {
 
-  var getLatest = function(request,reply,cb) {
-    request.server.plugins['hapi-mysql'].pool.getConnection(function(err, connection) {
-      if (err) {
-        console.log(err)
-        cb("DB connection failed (0003)")
-        return
-      }
+  getIndex(request,reply,function(er,index) {
 
-      var sql = "select * from content order by created desc limit 1"
+    var firstItem = index[0].slug
+    getItem(request,firstItem,function(er,latest) {
 
-      // Use the connection
-      connection.query(
-        sql,
-        // bindValues,
-        function(er, rows) {
+      if (er) throw new Error(er)
 
-          console.log("queried stuff")
+      reply.view('welcome',{
+        title: "Welcome!",
+        entry: latest,
+        index: index
+      })
 
-          if(er) {
-            cb( "content query failed (0001)")
-          } else {
-            if (rows.length == 0) {
-              cb("no content found (0002)")
-            } else {
-              var result = rows[0]
-              cb(null,result)
-            }
-          }
-
-          // And done with the connection.
-          connection.release()
-        }
-      )
-    })
-  }
-
-  var getIndex = function(request,reply,cb) {
-    console.log("index")
-    request.server.plugins['hapi-mysql'].pool.getConnection(function(err, connection) {
-
-      if (err) {
-        cb("DB connection failed (0004)")
-        return
-      }
-
-      var sql = "select title, slug, published from content order by created desc limit 50"
-
-      // Use the connection
-      connection.query(
-        sql,
-        // bindValues,
-        function(er, rows) {
-
-          if(er) {
-            cb( "content query failed (0005)")
-          } else {
-            if (rows.length == 0) {
-              cb("no content found (0006)")
-            } else {
-              cb(null,rows)
-            }
-          }
-
-          // And done with the connection.
-          connection.release()
-        }
-      )
-    })
-
-  }
-
-  async.series({
-    latest: function(cb) { getLatest(request,reply,cb) },
-    index: function(cb) { getIndex(request,reply,cb) }
-  },function(er,results) {
-
-    reply.view('welcome',{
-      title: "Welcome!",
-      item: results.latest,
-      index: results.index
     })
 
   })
 }
 
 exports.single = function(request,reply) {
+
+  getItem(request,request.params.slug,function(er,item) {
+
+    if (er) throw new Error(er)
+
+    reply.view('single',{
+      item: item
+    })
+
+  })
 
 }
